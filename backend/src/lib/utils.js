1e9b58e24853f109d44f12a99c402a5ceadbfeb5
Abdullah Al Taieb <abdullah.al.taieb.02@gmail.com>
2026-09-23 01:38:38 +0600

    signup section code refactoring

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,7 +1,14 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+  const { JWT_SECRET } = process.env;
+  if (!JWT_SECRET) {
+    throw new Error(
+      "JWT_SECRET is not configured in the environment variables",
+    );
+  }
+
+  const token = jwt.sign({ userId }, JWT_SECRET, {
     expiresIn: "7d",
   });
   res.cookie("jwt", token, {
