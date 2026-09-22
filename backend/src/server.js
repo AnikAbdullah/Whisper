fb7b1ba290b9617830ab34309e83a35f2e318316
Abdullah Al Taieb <abdullah.al.taieb.02@gmail.com>
2026-09-23 02:47:56 +0600

    welcome email implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,14 @@
 import express from "express";
-import dotenv from "dotenv";
+import { ENV } from "./lib/env.js";
 import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import connectDB from "./lib/db.js";
 
-dotenv.config();
 const __dirname = path.resolve();
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = ENV.PORT || 3000;
 app.use(express.json());
 
 // middlewares
@@ -20,7 +19,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
-if (process.env.NODE_ENV === "production") {
+if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.use((_, res) => {
